2c210f11738861890f62952a0b1be56dacd7b417
Christopher Lugod <christianlugod05@gmail.com>
2019-01-31 06:43:00 -0500

    handled auth

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const bodyParser = require('body-parser');
-
+const {mongoURI} = require('./config/keys');
 const app = express();
+
+// Passport config
+require('./config/passport')(passport);
+
+// load routes
+const auth = require('./routes/oauth');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,6 +20,10 @@ app.use(bodyParser.json());
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
+
+// Use routes
+
+app.use('/auth',auth);
 
 const port = process.env.PORT || 5000; 
 
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: 'mongodb://topzthedev:topzthedev2559069@ds117145.mlab.com:17145/storybook-dev',
+    googleClientId: '683065257308-462be22rsbvp5poqmvqdl8vlk775nh84.apps.googleusercontent.com',
+    googleClientSecret: 'pI8zLBbL0aCCQ4X3SKfE88K2'
+
+}
